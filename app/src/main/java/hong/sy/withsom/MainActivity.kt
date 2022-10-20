a06aa48d0152464757439e83b6c34182ef1f612d
seoy9 <ghfdwe@gmail.com>
2022-10-20 14:21:24 +0900

    First commit to connect with GitHub

--- app/src/main/java/hong/sy/withsom/MainActivity.kt
+++ app/src/main/java/hong/sy/withsom/MainActivity.kt
@@ -6,8 +6,17 @@ import androidx.viewpager2.widget.ViewPager2
 import hong.sy.withsom.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding.viewpagerNotice.adapter = NoticeViewPagerAdapter(getNoticeList())
+        binding.viewpagerNotice.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+    }
+
+    private fun getNoticeList(): ArrayList<Int> {
+        return arrayListOf<Int>(R.drawable.notice_banner1, R.drawable.notice_banner2, R.drawable.notice_banner3)
     }
 }
